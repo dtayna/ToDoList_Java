0945237ea6f6676db7a83c2b9d89bacc9449db23
Debora Tayna <dtayna.3194@gmail.com>
2022-02-13 21:24:07 -0300

    anotacoes pronta

--- src/main/java/com/aulas/ProjetoU2/beans/AnotacaoMBean.java
+++ src/main/java/com/aulas/ProjetoU2/beans/AnotacaoMBean.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.AjaxBehaviorEvent;
 
 import com.aulas.ProjetoU2.dao.AnotacaoDAO;
+import com.aulas.ProjetoU2.dao.TarefaDAO;
 import com.aulas.ProjetoU2.dominio.Anotacao;
-import com.aulas.ProjetoU2.dominio.Responsavel;
+import com.aulas.ProjetoU2.dominio.Tarefa;
 import com.aulas.ProjetoU2.util.ProjetoU2Exception;
 
 @ManagedBean
@@ -24,6 +26,32 @@ public class AnotacaoMBean implements Serializable {
 	Anotacao anotacao = new Anotacao();
 	AnotacaoDAO anotacaoDAO = new AnotacaoDAO();
 	
+	private TarefaDAO tarefaDAO = new TarefaDAO();
+	private Integer tarefaId;
+	private Tarefa tarefa;
+	
+	
+
+	public Integer getTarefaId() {
+		return tarefaId;
+	}
+
+	public void setTarefaId(Integer tarefaId) {
+		this.tarefaId = tarefaId;
+	}
+
+	public Tarefa getTarefa() {
+		return tarefa;
+	}
+
+	public void setTarefa(Tarefa tarefa) {
+		this.tarefa = tarefa;
+	}
+
+	public void atualizaTarefa(AjaxBehaviorEvent e) throws ProjetoU2Exception {
+		this.tarefa = tarefaDAO.buscarId(tarefaId);
+	}
+	
 	Date date = new Date();
 	
 		//APAGAR
@@ -37,19 +65,31 @@ public class AnotacaoMBean implements Serializable {
 		}
 		
 		//GRAVAR
-		public void editar(Anotacao anotacao) {
+		public void editar(Anotacao anotacao) throws ProjetoU2Exception{
+			atualizaEditar(anotacao);
 			this.anotacao = anotacao;
 		}
 		
+		public void atualizaEditar(Anotacao anotacao) throws ProjetoU2Exception {
+			tarefaId = anotacao.getTarefa().getId();
+			
+		}
 		
 		public void gravar() throws ProjetoU2Exception {
 			anotacao.setData(this.date);
 			System.out.println("data " +date);
+			for (Tarefa p : tarefaDAO.listar()) {
+				if (p.getId() == tarefaId) {
+					this.anotacao.setTarefa(p);
+						break;
+					}
+				}
 			if(this.anotacao.getId() == null) {
 				anotacaoDAO.salvar(this.anotacao);
 			} else {
 				anotacaoDAO.atualizar(this.anotacao);
 			}
+			tarefaId = null;
 			anotacao = new Anotacao();
 		}
 
